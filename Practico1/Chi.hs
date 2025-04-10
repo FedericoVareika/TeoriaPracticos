f63ae82b46434b9e068a2d59bdcae5a6aa930777
Federico Vareika <fede.vareika@gmail.com>
2025-04-10 20:32:37 -0300

    parte 5 sin testear

--- Practico1/Chi.hs
+++ Practico1/Chi.hs
@@ -3,6 +3,7 @@
 module Chi where 
 
 import LookupTable
+import Distribution.Simple.Program.HcPkg (list)
 
 type K = String --Constructor 
 type X = String --Variable 
@@ -27,6 +28,9 @@ data W = ConsW K [E]
 
 type S = Table K E 
 
+buscarRama :: K -> [B] -> Maybe B 
+buscarRama k = find (\Rama k' es' -> k == k') 
+
 efectoSRama :: S -> B -> B 
 efectoSRama s (Rama k xs e) = let s' = foldr del s xs in
     Rama k xs $ efectoS s' e
@@ -38,5 +42,24 @@ efectoS s (Var x) = case lkup x s of
 efectoS s (Cons k es) = Cons k $ map (efectoS s) es 
 efectoS s (Lambda x e) = Lambda x $ efectoS (del x s) e
 efectoS s (Ap e1 e2) = Ap (efectoS s e1) (efectoS s e2)
-efectoS s (Case e bs)  = Case (efectoS s e) $ map (efectoSRama s) bs   
+efectoS s (Case e bs) = Case (efectoS s e) $ map (efectoSRama s) bs   
+efectoS s (Rec x e) = Rec x $ efectoS (del x s) e 
 
+listsToTuples :: [a] -> [b] -> [(a, b)]
+listsToTuples [] ys = [] 
+listsToTuples xs [] = [] 
+listsToTuples (x:xs) (y:ys) = (x, y) : listsToTuples xs ys 
+
+evalParcial :: E -> W 
+evalParcial (Cons k es) = ConsW k es 
+evalParcial (Lambda x e) = LambdaW x e
+evalParcial (Ap e e') = case evalParcial e of 
+    LambdaW x e'' -> evalParcial $ efectoS [(x, e')] e''  
+    ConsW k es -> ConsW k (es ++ [e'])
+evalParcial (Case e bs) = case evalParcial e of 
+    LambdaW _ _ -> error "Expresion del case debe evaluarse a un constructor" 
+    ConsW k es -> case buscarRama k bs of 
+        Just (Rama k' xs e') -> assert (length es == length xs) $
+            evalParcial $ efectoS (listsToTuples xs es) e'
+        Nothing -> error "No se logro cubrir el caso para el constructor: " ++ k
+evalParcial (Rec x e) = evalParcial $ efectoS [(x, Rec x e)] e 
